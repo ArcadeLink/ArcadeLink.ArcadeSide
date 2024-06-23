31d1ccaf75cbee9601f8fdb07c858b0bf5aa82fe
Hina <haotrip05@gmail.com>
2024-06-23 18:35:34 +0800

    modify: 将设置做到命令行中，移除设置界面

--- Assets/Scripts/Managers/SettingsManager.cs
+++ Assets/Scripts/Managers/SettingsManager.cs
@@ -1,15 +1,17 @@
 using System.Collections.Generic;
 using System.IO;
+using ANU.IngameDebug.Console;
 using Models;
 using Newtonsoft.Json;
 using UnityEngine;
 
 namespace Managers
 {
+    [DebugCommandPrefix("config")]
     public class SettingsManager : MonoBehaviour
     {
         public static SettingsManager Instance { get; private set; }
-        public List<WebCamDevice> CameraDevices = new List<WebCamDevice>();
+        public readonly List<WebCamDevice> CameraDevices = new();
 
         private string _settingsPath;
 
@@ -39,6 +41,104 @@ namespace Managers
                 File.WriteAllText(_settingsPath, JsonConvert.SerializeObject(new SettingsModel()));
             }
             Instance = this;
+        }
+        
+        [DebugCommand("get")]
+        public void GetSettings(string key)
+        {
+            var settings = Settings;
+            switch (key)
+            {
+                case "input":
+                    // TODO: Implement input getting logic
+                    break;
+                case "serial":
+                    // TODO: Implement serial getting logic
+                    break;
+                case "api_addr":
+                    Debug.Log(settings.ApiEndpoint);
+                    break;
+                case "location":
+                    Debug.Log($"{settings.LocationId}:{settings.LocationKey}");
+                    break;
+                case "qiniu_bucket":
+                    Debug.Log(settings.BucketName);
+                    break;
+                case "qiniu_cred":
+                    Debug.Log($"{settings.QiniuAk}:{settings.QiniuSk}");
+                    break;
+                case "file_url":
+                    Debug.Log(settings.FileUrl);
+                    break;
+                case "record_interval":
+                    Debug.Log(settings.RecordInterval.ToString());
+                    break;
+                case "ffmpeg_executable":
+                    Debug.Log(settings.FFmpegPath);
+                    break;
+                case "ffmpeg_hw":
+                    // TODO: Implement ffmpeg_hw getting logic
+                    break;
+                case "all":
+                    Debug.Log(JsonConvert.SerializeObject(settings));
+                    break;
+                default:
+                    Debug.Log($"Unknown setting key: {key}");
+                    break;
+            }
+        }
+        
+        [DebugCommand("set")]
+        public void SetSettings(string key, string value)
+        {
+            var settings = Settings;
+            switch (key)
+            {
+                case "input":
+                    // TODO: Implement input setting logic
+                    break;
+                case "serial":
+                    // TODO: Implement serial setting logic
+                    break;
+                case "api_addr":
+                    settings.ApiEndpoint = value;
+                    break;
+                case "location":
+                    var locationParts = value.Split(':');
+                    if (locationParts.Length == 2)
+                    {
+                        settings.LocationId = int.Parse(locationParts[0]);
+                        settings.LocationKey = locationParts[1];
+                    }
+                    break;
+                case "qiniu_bucket":
+                    settings.BucketName = value;
+                    break;
+                case "qiniu_cred":
+                    var qiniuParts = value.Split(':');
+                    if (qiniuParts.Length == 2)
+                    {
+                        settings.QiniuAk = qiniuParts[0];
+                        settings.QiniuSk = qiniuParts[1];
+                    }
+                    break;
+                case "file_url":
+                    settings.FileUrl = value;
+                    break;
+                case "record_interval":
+                    settings.RecordInterval = float.Parse(value);
+                    break;
+                case "ffmpeg_executable":
+                    settings.FFmpegPath = value;
+                    break;
+                case "ffmpeg_hw":
+                    // TODO: Implement ffmpeg_hw setting logic
+                    break;
+                default:
+                    Debug.Log($"Unknown setting key: {key}");
+                    break;
+            }
+            Settings = settings;
         }
     }
 }
--- Assets/Scripts/SettingsUIController.cs
+++ Assets/Scripts/SettingsUIController.cs
@@ -8,61 +8,28 @@ using UnityEngine.UI;
 public class SettingsUIController : MonoBehaviour
 {
     [SerializeField]
-    private TMP_InputField locationIdInputField, locationKeyInputField, apiEndpointInputField,
-        qiniuAccessKeyInputField, qiniuSecretKeyInputField, qiniuBucketInputField, qiniuDomainInputField, recordIntervalInputField,
-            ffmpegBinaryPathInputField, ffmpegTempPathInputField;
-    [SerializeField]
-    private TMP_Dropdown cameraDropdown;
-    [SerializeField]
-    private Toggle maintanceModeToggle;
+    private TMP_Dropdown cameraDropdown, inputMethodDropdown;
     [SerializeField]
     private RawImage cameraPreviewRawImage;
-    
-    public void OnSaveButtonClicked()
-    {
-        var settings = SettingsManager.Instance.Settings;
-        settings.ApiEndpoint = apiEndpointInputField.text;
-        settings.LocationId = Convert.ToInt32(locationIdInputField.text);
-        settings.LocationKey = locationKeyInputField.text;
-        settings.CameraDevice = cameraDropdown.value;
-        settings.BucketName = qiniuBucketInputField.text;
-        settings.QiniuAk = qiniuAccessKeyInputField.text;
-        settings.QiniuSk = qiniuSecretKeyInputField.text;
-        settings.FileUrl = qiniuDomainInputField.text;
-        settings.MaintainceMode = maintanceModeToggle.isOn;
-        settings.RecordInterval = float.Parse(recordIntervalInputField.text);
-        settings.FFmpegPath = ffmpegBinaryPathInputField.text;
-        settings.FFmpegTempPath = ffmpegTempPathInputField.text;
-        
-        SettingsManager.Instance.Settings = settings;
-
-        OnBackButtonClicked();
-    }
 
     private void Start()
     {
         var settings = SettingsManager.Instance.Settings;
-        locationIdInputField.text = settings.LocationId.ToString();
-        locationKeyInputField.text = settings.LocationKey;
         cameraDropdown.value = settings.CameraDevice;
-        qiniuBucketInputField.text = settings.BucketName;
-        qiniuAccessKeyInputField.text = settings.QiniuAk;
-        qiniuSecretKeyInputField.text = settings.QiniuSk;
-        qiniuDomainInputField.text = settings.FileUrl;
-        apiEndpointInputField.text = settings.ApiEndpoint;
-        maintanceModeToggle.isOn = settings.MaintainceMode;
-        recordIntervalInputField.text = settings.RecordInterval.ToString(CultureInfo.InvariantCulture);
-        ffmpegBinaryPathInputField.text = settings.FFmpegPath;
-        ffmpegTempPathInputField.text = settings.FFmpegTempPath;
-        
         var devices = WebCamTexture.devices;
         foreach (var device in devices)
         {
             cameraDropdown.options.Add(new TMP_Dropdown.OptionData(device.name));
         }
-        
         cameraDropdown.RefreshShownValue();
-        
+        cameraDropdown.onValueChanged.AddListener(index =>
+        {
+            settings.CameraDevice = index;
+            SettingsManager.Instance.Settings = settings;
+            var webCamTexture = new WebCamTexture(devices[index].name);
+            cameraPreviewRawImage.texture = webCamTexture;
+            webCamTexture.Play();
+        });
         var webCamTexture = new WebCamTexture(devices[settings.CameraDevice].name);
         cameraPreviewRawImage.texture = webCamTexture;
         webCamTexture.Play();
@@ -78,12 +45,8 @@ public class SettingsUIController : MonoBehaviour
     {
         if (Input.GetButton("Cancel"))
         {
-            OnSaveButtonClicked();
-        }
-    }
+            StateManager.Instance.SetState(States.Main_Window);
 
-    private void OnBackButtonClicked()
-    {
-        StateManager.Instance.SetState(States.Main_Window);
+        }
     }
 }
